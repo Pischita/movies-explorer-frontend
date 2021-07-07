af20b6513f3977bfb8bdcb80eac333ed6e97f144
Ivan <i.pischita@sviat.by>
2021-07-07 11:19:30 +0300

    Сделать валидацию формы логина

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,80 @@
 import './Login.css';
 import Header from '../Header/Header';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import * as mainApi from '../../utils/MainApi';
-import { useHistory } from 'react-router-dom';
 
 export default function Login({onSubmit, errorMessage}) {
+    
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const history = useHistory();
+
+    const [emailDirty, setEmailDirty] = useState(false);
+    const [errorEmail, setErrorEmail] = useState('');
+
+    const [passwordDirty, setPasswordDirty] = useState(false);
+    const [errorPassword, setErrorPassword] = useState('');
+
+    const [disableSubmit, setDisableSubmit] = useState(true);
+
+    useEffect(()=>{
+        if(!emailDirty && !passwordDirty){
+          setDisableSubmit(false);
+        }else{
+          setDisableSubmit(true);
+        }
+        console.log(disableSubmit);
+      }, [passwordDirty, emailDirty, password, email]);
+    
+      useEffect(()=>{
+        setDisableSubmit(true);
+      }, []);
+    
+
+
+    function handleBlur(evt){
+        switch (evt.target.name) {
+            case 'email':
+                setEmailDirty(true);
+                break;
+            case 'password':
+                setPasswordDirty(true);
+                break;
+            
+            default:
+                break;
+        }
+    }
+
 
     function handleEmailChange(evt){
-        setEmail(evt.target.value);
+        const value = evt.target.value;
+        setEmail(value);
+    
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+       
+        if(value.length < 2){
+          setErrorEmail('Введите email');
+          setEmailDirty(true);
+        } else if( ! re.test(String(value).toLowerCase()) ){
+          setErrorEmail('Email не корректный');
+          setEmailDirty(true);
+        } else {
+          setErrorEmail('')
+          setEmailDirty(false);
+        }  
     }
 
     function handlePasswordChange(evt){
-        setPassword(evt.target.value);
+        const value = evt.target.value;
+        setPassword(value);
+
+        if(value.length <= 2){
+            setErrorPassword('Пароль должен быть больше 2х символов');
+            setPasswordDirty(true);
+        }else{
+            setErrorPassword('');
+            setPasswordDirty(false);
+        }
     } 
 
     function handleSubmit(evt){
@@ -31,16 +90,19 @@ export default function Login({onSubmit, errorMessage}) {
                
                 <div className="login__input-group">
                     <label className="login__label" htmlFor="email" >E-mail</label>
-                    <input className="login__input" id="email" type="email" name="email" value={email} onChange={handleEmailChange} required />
+                    <input className="login__input" id="email" type="email" name="email" value={email} onChange={handleEmailChange} onBlur={handleBlur} required />
                 </div>
+                {emailDirty && <p className="login__input-error">{errorEmail}</p>}
 
                 <div className="login__input-group">
                     <label className="login__label" htmlFor="password" >Password</label>
                     <input className="login__input" id="password" type="password" name="password" value={password} onChange={handlePasswordChange} required />
                 </div>
+                {passwordDirty && <p className="login__input-error">{errorPassword}</p>}
+                
                 <p className='login__error'>{errorMessage}</p>
 
-                <button className="login__submit" type="submit">Войти</button>
+                <button className="login__submit" type="submit" disabled={disableSubmit}>Войти</button>
             </form>
             <div className="login__register-wrapper">
                 <p className="login__register-text">Еще не зарегистрированы?</p>
