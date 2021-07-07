2b8c367a644dd205a8e5d75d03b14f006ecaabe7
Ivan <i.pischita@sviat.by>
2021-07-07 13:40:38 +0300

    Валидация формы при регистрации

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,6 @@
 import './Register.css';
 import Header from '../Header/Header';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { register, login } from '../../utils/MainApi';
 import { useHistory } from 'react-router-dom';
@@ -12,16 +12,78 @@ export default function Register({ onSubmit }) {
   const history = useHistory();
   const [errorMessage, setErrorMessage] = useState('');
 
+  const [nameDirty, setNameDirty] = useState(false);
+  const [errorName, setErrorName] = useState('Имя должно быть заполнено');
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [errorEmail, setErrorEmail] = useState('Email должен быть заполнен');
+
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [errorPassword, setErrorPassword] = useState('Укажите пароль');
+
+  const [disableSubmit, setDisableSubmit] = useState(true);
+
+  useEffect(() => {
+    if (!emailDirty && !passwordDirty && !nameDirty) {
+      setDisableSubmit(false);
+    } else {
+      setDisableSubmit(true);
+    }
+
+    console.log(disableSubmit);
+
+  }, [passwordDirty, emailDirty, password, email, nameDirty, name]);
+
+  useEffect(() => {
+    setDisableSubmit(true);
+  }, []);
+
+
+
+  function handleBlur(evt) {
+    
+  }
+
   function handleNameChange(evt) {
+    const value = evt.target.value;
     setName(evt.target.value);
+    if(value.length < 2){
+      setErrorName('Введите имя');
+      setNameDirty(true);
+    } else {
+      setErrorName('')
+      setNameDirty(false);
+    }
   }
 
   function handleEmailChange(evt) {
-    setEmail(evt.target.value);
+    const value = evt.target.value;
+    setEmail(value);
+
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (value.length < 2) {
+      setErrorEmail('Введите email');
+      setEmailDirty(true);
+    } else if (!re.test(String(value).toLowerCase())) {
+      setErrorEmail('Email не корректный');
+      setEmailDirty(true);
+    } else {
+      setErrorEmail('')
+      setEmailDirty(false);
+    }
   }
 
   function handlePasswordChange(evt) {
-    setPassword(evt.target.value);
+    const value = evt.target.value;
+    setPassword(value);
+
+    if(value.length <= 2){
+        setErrorPassword('Пароль должен быть больше 2х символов');
+        setPasswordDirty(true);
+    }else{
+        setErrorPassword('');
+        setPasswordDirty(false);
+    }
   }
 
   function handleSubmit(evt) {
@@ -57,9 +119,11 @@ export default function Register({ onSubmit }) {
             name='name'
             value={name}
             onChange={handleNameChange}
+            onBlur={handleBlur}
             required
           />
         </div>
+        {nameDirty && <p className="profile__input-error">{errorName}</p>}
 
         <div className='register__input-group'>
           <label className='register__label' htmlFor='email'>
@@ -72,9 +136,11 @@ export default function Register({ onSubmit }) {
             name='email'
             value={email}
             onChange={handleEmailChange}
+            onBlur={handleBlur}
             required
           />
         </div>
+        {emailDirty && <p className="register__input-error">{errorEmail}</p>}
 
         <div className='register__input-group'>
           <label className='register__label' htmlFor='password'>
@@ -87,12 +153,14 @@ export default function Register({ onSubmit }) {
             name='password'
             value={password}
             onChange={handlePasswordChange}
+            onBlur={handleBlur}
             required
           />
         </div>
+        {passwordDirty && <p className="register__input-error">{errorPassword}</p>}
         <p className='register__error'>{errorMessage}</p>
 
-        <button className='register__submit' type='submit'>
+        <button className='register__submit' type='submit' disabled={disableSubmit}>
           Зарегистрироваться
         </button>
       </form>
